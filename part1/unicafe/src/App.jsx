ba6b17f1ef4567a80c1104e34b69bf456c1bc0f8
Veeti Martinmäki <181813689+veetimar@users.noreply.github.com>
2026-08-15 00:45:14 +0300

    1.7

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -5,6 +5,7 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const total = good + neutral + bad
 
   return (
     <div>
@@ -16,6 +17,9 @@ const App = () => {
       <p>good {good}</p>
       <p>neutral {neutral}</p>
       <p>bad {bad}</p>
+      <p>all {total}</p>
+      <p>average {(good - bad) / (total)}</p>
+      <p>positive {good / total * 100}%</p>
     </div>
   )
 }
