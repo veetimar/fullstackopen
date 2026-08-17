85b70c067f2aeb35d33413ca5de205c03547cb29
Veeti Martinmäki <181813689+veetimar@users.noreply.github.com>
2026-08-17 14:52:59 +0300

    2.5

---
+++ part2/courseinfo/src/components/Course.jsx
@@ -0,0 +1,38 @@
+const Header = (props) => <h2>{props.course}</h2>
+
+const Content = ({ parts }) => (
+  <div>
+    {parts.map((part) => (
+      <Part key={part.id} part={part} />
+    ))}
+  </div>
+)
+
+const Part = (props) => (
+  <p>
+    {props.part.name} {props.part.exercises}
+  </p>
+)
+
+const Total = ({ parts }) => {
+
+  const total = parts.reduce((sum, part) => sum + part.exercises, 0)
+  
+  return (
+    <p>
+      <b>total of {total} exercises</b>
+    </p>
+  )
+}
+
+const Course = ({ course }) => {
+  return (
+    <div>
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
+    </div>
+  )
+}
+
+export default Course
