bba6f21073b15a9c1301faeaf5ef2c7d36162ce5
Veeti Martinmäki <181813689+veetimar@users.noreply.github.com>
2026-08-26 17:58:34 +0300

    3.13

--- part3/phonebook-back/index.js
+++ part3/phonebook-back/index.js
@@ -1,5 +1,7 @@
+require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
+const Person = require('./models/person')
 const app = express()
 
 app.use(express.json())
@@ -19,40 +21,19 @@ app.use(morgan((tokens, req, res) => {
 
 app.use(express.static('dist'))
 
-let persons = [
-    { 
-      "id": "1",
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": "2",
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": "3",
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": "4",
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
-]
-
 app.get('/api/persons', (req, res) => {
-    res.json(persons)
+    Person.find({}).then(result => {
+        res.json(result)
+    })
 })
 
 app.get('/api/persons/:id', (req, res) => {
     const id = req.params.id
-    const person = persons.find((person) => person.id === id)
-    if (!person) {
-        return res.status(404).end()
-    }
-    res.json(person)
+    Person.findById(id).then(result => {
+        res.json(result)
+    }).catch(e => {
+        res.status(404).end()
+    })
 })
 
 app.get('/info', (req, res) => {
@@ -90,7 +71,7 @@ app.post('/api/persons', (req, res) => {
     res.json(person)
 })
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
